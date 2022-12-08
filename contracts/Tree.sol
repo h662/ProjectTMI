31f0b3519febaf336f3b4af02699d4710d486af5
h662Olbm <h662@olbm.app>
2022-12-08 18:55:11 +0900

    smart contract

--- contracts/Tree.sol
+++ contracts/Tree.sol
@@ -4,31 +4,32 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-
-import './Berry.sol';
+import "./Berry.sol";
+import "./Ground.sol";
 
 contract Tree is ERC721Enumerable, Ownable {
     struct TreeData {
         uint level;
-        string color;
-        string shape;
-        uint berryAmount;
-        uint exp;
+        uint[] berryId;
+        uint[] berryAmount;
+        uint[] requireBerryId;
+        uint[] requireBerryAmount;
         bool isMaxLevel;
+        uint nextTreeId;
     }
     
     mapping(uint => uint) trees;
-    mapping(uint => TreeData) treeData;
-    // 변수명
+    mapping(uint => TreeData) public treeData;
     mapping(uint => uint) bearTime; 
-
-    string metadataURI;
+    mapping(uint => string) metadataUri;
+    mapping(uint => bool) public isPlanted;
 
     address berryAddress;
     Berry berryContract;
+    address groundAddress;
+    Ground groundContract;
 
-    uint public treeDataId;
+    uint treeDataId;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
@@ -37,8 +38,13 @@ contract Tree is ERC721Enumerable, Ownable {
         _;
     }
 
+    modifier isTransferable(uint _tokenId) {
+        require(!isPlanted[_tokenId], "Already planted");
+        _;
+    }
+
     function mintTree(uint _treeDataId) public onlyOwner {
-        require(treeData[_treeDataId].berryAmount > 0, "Not exist tree.");
+        require(treeData[_treeDataId].berryAmount[0] > 0, "Not exist tree.");
         require(treeData[_treeDataId].level == 1, "Not level 1.");
 
         uint tokenId = totalSupply() + 1;
@@ -48,40 +54,47 @@ contract Tree is ERC721Enumerable, Ownable {
         _mint(msg.sender, tokenId);
     }
 
-    function setTreeData(string memory _color, string memory _shape, uint[] memory _berryAmount, uint[] memory _exp) public onlyOwner {
-        require(_berryAmount.length == _exp.length, "Input wrong.");
-        require(_exp[_exp.length - 1] == 0, "Max level tree is exp not 0.");
+    function setTreeData(
+        uint _level,
+        uint[] memory _berryId, 
+        uint[] memory _berryAmount, 
+        uint[] memory _requireBerryId, 
+        uint[] memory _requireBerryAmount,
+        bool _isMaxLevel,
+        uint _nextTreeId,
+        string memory _metadataUri
+        ) public onlyOwner {
+            require(_berryId.length == _berryAmount.length, "Not equal _berryId & _berryAmount length.");
+            require(_requireBerryId.length == _requireBerryAmount.length, "Not equal _requireBerryId & _requireBerryAmount length.");
 
-        for(uint i = 0; i < _berryAmount.length; i++) {
             treeDataId++;
-            treeData[treeDataId] = TreeData(i + 1, _color, _shape, _berryAmount[i], _exp[i] * 10 ** 18, false);
 
-            if(i == _berryAmount.length - 1) {
-                treeData[treeDataId].isMaxLevel = true;
-            }
-        }
-    }
-
-    function setTokenURI(string memory _metadataURI) public onlyOwner {
-        metadataURI = _metadataURI;
+            treeData[treeDataId] = TreeData(
+                _level, 
+                _berryId, 
+                _berryAmount, 
+                _requireBerryId, 
+                _requireBerryAmount, 
+                _isMaxLevel,
+                _nextTreeId
+            );
+            
+            metadataUri[treeDataId] = _metadataUri;
     }
 
     function tokenURI(uint _tokenId) public override view returns(string memory) {
         uint _treeDataId = trees[_tokenId];
 
-        uint level = treeData[_treeDataId].level;
-        string memory color = treeData[_treeDataId].color;
-        string memory shape = treeData[_treeDataId].shape;
-
-        return string(abi.encodePacked(metadataURI, '/', shape, '/', color, '/', Strings.toString(level), '.json'));
+        return metadataUri[_treeDataId];
     }
 
-    function getBerryAmount(uint _tokenId) public view returns(uint) {
+    function getBerryAmount(uint _tokenId) public view returns(uint[] memory, uint[] memory) {
         uint _treeDataId = trees[_tokenId];
 
-        uint _berryAmount = treeData[_treeDataId].berryAmount;
+        uint[] memory _berryId = treeData[_treeDataId].berryId;
+        uint[] memory _berryAmount = treeData[_treeDataId].berryAmount;
 
-        return _berryAmount;
+        return (_berryId, _berryAmount);
     }
 
     function setBearTime(uint _tokenId, uint _bearTime) public {
@@ -98,14 +111,75 @@ contract Tree is ERC721Enumerable, Ownable {
         berryAddress = _berryAddress;
         berryContract = Berry(_berryAddress);
     }
+    function setGroundContract(address _groundAddress) public onlyOwner {
+        groundAddress = _groundAddress;
+        groundContract = Ground(_groundAddress);
+    }
+
 
     function levelUp(uint _tokenId) public treeOwner(_tokenId) {
         uint _treeDataId = trees[_tokenId];
 
         require(!treeData[_treeDataId].isMaxLevel, "Max level.");
-        require(treeData[_treeDataId].exp <= berryContract.balanceOf(msg.sender), "Not enough berry.");
+        require(berryCheck(_treeDataId), "Not enough berry.");
 
-        berryContract.burnBerry(msg.sender, treeData[_treeDataId].exp);
-        trees[_tokenId] = _treeDataId + 1;
+        berryContract.burnBerry(msg.sender, treeData[_treeDataId].requireBerryId, treeData[_treeDataId].requireBerryAmount);
+        trees[_tokenId] = treeData[_treeDataId].nextTreeId;
+    }
+
+    function berryCheck(uint _treeDataId) public view returns(bool) {
+        bool result = true;
+
+        for(uint i = 0; i < treeData[_treeDataId].requireBerryId.length; i++) {
+            if(treeData[_treeDataId].requireBerryAmount[i] > berryContract.balanceOf(msg.sender, treeData[_treeDataId].requireBerryId[i])) {
+                result = false;
+            }
+        }
+
+        return result;
+    }
+
+    function plantTree(address _groundOwner, uint _tokenId) public {
+        require(msg.sender == groundAddress, "Caller is not ground contract.");
+        require(!isPlanted[_tokenId], "Already planted.");
+        require(ownerOf(_tokenId) == _groundOwner, "U R not tree owner.");
+
+        isPlanted[_tokenId] = true;
+    }
+
+    function popTree(address _groundOwner, uint _tokenId) public {
+        require(msg.sender == groundAddress, "Caller is not ground contract");
+        require(isPlanted[_tokenId], "Not planted.");
+        require(ownerOf(_tokenId) == _groundOwner, "U R not tree owner.");
+
+        isPlanted[_tokenId] = false;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override isTransferable(tokenId) {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
+
+        _transfer(from, to, tokenId);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override isTransferable(tokenId) {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public override isTransferable(tokenId) {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
+        _safeTransfer(from, to, tokenId, data);
     }
 }
